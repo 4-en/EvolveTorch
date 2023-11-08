ccaf203f19cd7d29a4a96d4e731c7db0018321ec
Ben Schumacher <benschumacher0@gmail.com>
2023-11-08 20:11:40 +0900

    first prototype

--- evolve.py
+++ evolve.py
@@ -4,6 +4,9 @@ import numpy as np
 import base64
 import random
 import torch
+import tqdm
+
+import matplotlib.pyplot as plt
 
 class DNA:
     """
@@ -43,26 +46,95 @@ class DNA:
         
 
 class Genome:
-    def __init__(self, model: torch.nn.Module):
+    def __init__(self, model: torch.nn.Module, parents = [], dna = None, population = None):
         self.model = model
         self.fitness = 0.0
-        
-        self.dna = DNA()
+        if dna is not None:
+            self.dna = dna
+        else:
+            self.dna = DNA()
+        self.parents = parents
+        self.population = population
     
     def __str__(self) -> str:
         return f"Genome of {self.model.__class__.__name__} with DNA {self.dna}"
+    
+    def mutate(self, mutation_rate=0.01, mutation_amount=0.1)->"Genome":
+        """Returns a new instance of this genome with a mutated DNA and model"""
+        parents = [self.dna]
+        dna = self.dna.mutate()
+        model = self.population.factory()
+        # mutate the model
+        for param_old, param_new in zip(self.model.parameters(), model.parameters()):
+            chances = torch.rand(param_old.shape)
+            mask = chances < mutation_rate
+            mutations = torch.randn(param_old.shape) * mask * mutation_amount
+            param_new = param_old + mutations
+        return Genome(model, parents, dna, population=self.population)
+    
+    def crossover(self, other:"Genome")->"Genome":
+        """Returns a new instance of this genome with a crossover of the DNA and model"""
+        parents = [self.dna, other.dna]
+        dna = DNA()
+        model = self.population.factory()
+        # crossover the model
+        for param_self, param_other, param_new in zip(self.model.parameters(), other.model.parameters(), model.parameters()):
+            chances = torch.rand(param_self.shape)
+            mask = chances < 0.5
+            param_new = param_self * mask + param_other * (~mask)
+        return Genome(model, parents, dna, population=self.population)
     
     
 
 
 class Population:
-    def __init__(self, factory: callable, size: int = 128):
+    """
+    Represents a population of genomes.
+    
+    During training, each genome is evaluated and assigned a fitness score.
+    The genomes are then sorted by fitness and the top genomes are selected to create the next generation.
+    The genomes are then crossed over and mutated to create the next generation.
+    """
+
+    def __init__(self, factory: callable, fitness_function: callable, inversed_fitness=False, size: int = 128, weights: dict = None):
+        """
+        Create a population of genomes
+        
+        Parameters
+        ----------
+        factory : callable
+            a function that returns a new instance of a genome
+        fitness_function : callable
+            a function that returns the fitness of a genome
+        inversed_fitness : bool, optional
+            true if the fitness function returns a value that is better the lower it is, by default False
+        size : int, optional
+            the size of the population, by default 128
+        weights : dict, optional
+            the weights of the different operations to create a new generation, by default None
+        """
         self.factory = factory
+        self.fitness_function = fitness_function
         self.size = size
         self.generation = 0
+        self.keep_old_gen_n = 0 # number of old models to keep
+        self.top_p = 0.2 # percentage of top genomes to use for the next generation
+        self.top_weighting = lambda idx: 1.0 # weighting of the top genomes
+        self.generations = [] # old generations, usually without the models
         self.genomes = []
+        self.verbose = True # print progress
+        self.weights = {
+            "mutation": 2,
+            "crossover": 2,
+            "elitism": 1,
+            "random": 10
+        }
+        if weights is not None:
+            self.weights.update(weights)
 
         self.fill_population()
+
+        
 
     def __len__(self):
         return len(self.genomes)
@@ -85,42 +157,165 @@ class Population:
     def __repr__(self):
         return f"Population(factory={self.factory}, size={self.size})"
     
+    def set_weights(self, weights: dict):
+        """
+        Set the weights of the different operations to create a new generation
+        
+        keys:
+        - mutation
+        - crossover
+        - elitism
+        - random
+        """
+        self.weights.update(weights)
+
+    def _select_operation(self):
+        """
+        Returns an operation to create a new generation based on the weights"""
+        
+        keys = list(self.weights.keys())
+        weights = np.array(list(self.weights.values()))
+        weights = weights / weights.sum()
+        return np.random.choice(keys, p=weights)
+    
+    
+    def next_generation(self):
+        "create the next generation of genomes based on the current population"
+        self.generation += 1
+        old_genomes = self.genomes
+        self.genomes = []
+
+        # sort the genomes by fitness
+        old_genomes.sort(key=lambda g: g.fitness, reverse=True)
+
+        next_elitism = 0
+
+        top_pop = int(self.top_p * self.size)
+        top_pop = old_genomes[:top_pop]
+        top_weights = [self.top_weighting(i) for i in range(len(top_pop))]
+
+        for _ in range(self.size):
+            # select an operation
+            operation = self._select_operation()
+
+            new_genome = None
+
+
+            if operation == "random":
+                # create a random genome
+                new_genome = self.create_genome()
+            elif operation == "mutation":
+                # mutate a random genome
+                mutation_target = random.choices(top_pop, weights=top_weights)[0]
+                new_genome = mutation_target.mutate()
+            elif operation == "crossover":
+                # crossover two random genomes
+                parent1 = random.choices(top_pop, weights=top_weights)[0]
+                weight_no_parent1 = [w * int(parent1==top_pop[i]) for i, w in enumerate(top_weights)] # set the weight of parent1 to 0
+                parent2 = random.choices(top_pop, weights=weight_no_parent1)[0]
+                new_genome = parent1.crossover(parent2)
+            elif operation == "elitism":
+                # use the next genome in the top genomes
+                if next_elitism < len(top_pop):
+                    new_genome = top_pop[next_elitism]
+                    next_elitism += 1
+                else:
+                    new_genome = self.create_genome()
+            else:
+                raise ValueError(f"Unknown operation {operation}")
+            
+                
+            self.genomes.append(new_genome)
+
+        save_top = self.keep_old_gen_n
+        for genome in old_genomes:
+            if save_top > 0:
+                save_top -= 1
+                continue
+            #genome.model = None
+            # maybe use del ?
+
+        self.generations.append(old_genomes)
+
+
     def fill_population(self):
         missing = self.size - len(self.genomes)
         new_genomes = [self.create_genome() for _ in range(missing)]
         self.genomes.extend(new_genomes)
     
     def create_genome(self):
-        return Genome(self.factory())
+        return Genome(self.factory(), population=self)
+    
+    def _print(self, *args, **kwargs):
+        if self.verbose:
+            print(*args, **kwargs)
 
-    def evolve(self):
-        pass
+    def eval_fitness(self):
+        for genome in self.genomes:
+            genome.fitness = self.fitness_function(genome)
+    
+    def evolve(self, generations=1):
 
-    def crossover(self):
-        pass
+        if self.generation == 0:
+            self._print(f"Evaluating generation {self.generation}")
+            # evaluate the fitness of the genomes
+            for genome in tqdm.tqdm(self.genomes):
+                genome.fitness = self.fitness_function(genome)
 
-    def mutate(self):
-        pass
+        for i in range(generations):
 
-    def evaluate(self):
-        pass
+            self._print(f"Creating generation {self.generation+1}")
+            # create the next generation
+            self.next_generation()
 
-    def select(self):
-        pass
+            self._print(f"Evaluating generation {self.generation}")
+            # evaluate the fitness of the genomes
+            for genome in tqdm.tqdm(self.genomes):
+                genome.fitness = self.fitness_function(genome)
 
-    def get_best(self):
-        pass
+            # sort the genomes by fitness
+            self.genomes.sort(key=lambda g: g.fitness, reverse=True)
+            # print best fitness
+            self._print(f"Best fitness: {self.genomes[0].fitness}")
 
+    def plot_fitness(self):
+        plt.plot([g.fitness for g in self.genomes])
+        plt.show()
+
+
+        
+class TestModel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = torch.nn.Linear(1, 32)
+        self.ReLU = torch.nn.ReLU()
+        self.linear2 = torch.nn.Linear(32, 1)
+    
+    def forward(self, x):
+        x = self.linear(x)
+        x = self.ReLU(x)
+        x = self.linear2(x)
+        return x
+
+def fitness_func(genome: Genome):
+    x = torch.arange(0, 2*np.pi, 0.1)
+    x = x.reshape(-1, 1)
+    y = torch.cos(x)
+    y_hat = genome.model(x)
+    loss = torch.nn.functional.mse_loss(y_hat, y)
+    return 1.0/loss
 
 def tests():
     print("Running tests")
     print("Creating a population of 128 genomes")
-    p = Population(lambda: torch.nn.Linear(1, 1))
+    p = Population(lambda: TestModel(), fitness_func, size=128)
     print(p)
     print("Creating a genome")
-    g = Genome(torch.nn.Linear(1, 1))
+    g = Genome(TestModel())
     print(g)
     print("Done")
+
+    p.evolve(50)
 
 if __name__ == "__main__":
     tests()
